f17d3d7ec9666baf5afed94aa579844e2c0f7b2c
Mike <mturano@secondplacegames.com>
2017-11-11 19:51:02 -0600

    improved name generator

--- barbarian_names.py
+++ barbarian_names.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-from random import choice
+import random
 
-_prefixes = ('Uth','Threk','Kur','Ro','Ar','Co','Wulf','Kor','Hroth','Be','Tho','Yor','Gor','Go','Bo','Hun','Un','Ec')
-_suffixes = ('gar','nan','gorn','ruk','wor','tor','orn','kuk','ak','rak','laf','son','ferth')
+_prefixes = ('Mo','Here','Sig','Ae','Wig','Uth','Thr','Kur','Ro','Ar','Co','Wulf','Kor','Hroth','Be','Tho','Yor','Gor','Go','Bo','Be','Hun','Un','Ec','Hy')
+_suffixes = ('gar','nan','gorn','wor','tor','orn','yth','rak','laf','son','th','mund','hulf','ere','wulf','gd','eow','mond')
+_middles = ('o','e','sch','fer','gel','gth','ek','')
+
+middle_chance = 0.4
+
+forbidden = ['Beowulf', 'Hrothgar']
 
 def barb_name():
-    return choice(_prefixes) + choice(_suffixes)
+    if random.uniform(0,1) < middle_chance:
+        name = random.choice(_prefixes) + random.choice(_middles) + random.choice(_suffixes)
+    else:
+        name = random.choice(_prefixes) + random.choice(_suffixes)
+    while name in forbidden:
+        name = barb_name()
+    return name
+    
+if __name__ == '__main__':
+    repeats = 0
+    names = [barb_name() for i in range(0,100)]
+    for i, n in enumerate(names):
+        if n in names[:i]:
+            repeats += 1
+        print(n)
+    print(str(repeats) + ' repeats')
